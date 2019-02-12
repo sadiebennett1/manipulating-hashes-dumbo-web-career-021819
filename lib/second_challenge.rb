9b5bdf2cd797bd00dff01056426d5ae6ca9a6a93
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-12 22:14:40 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,13 @@ def second_challenge
   }
 
   #code your solution here!
-
-  
+  vals = groceries.values
+  new_arr = []
+  vals.each do |arr|
+    arr.each do |word|
+      new_arr << word
+    end
+  end
+  new_arr
 
 end
